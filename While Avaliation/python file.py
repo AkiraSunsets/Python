62e76d7cd5dfb9997a84dabb07e96af67dc4d944
Akira Sunsets <132906123+AkiraSunsets@users.noreply.github.com>
2025-04-08 16:41:01 -0300

    Update python file.py

--- While Avaliation/python file.py
+++ While Avaliation/python file.py
@@ -47,4 +47,20 @@ while opcao != 4:
         print("Opção inválida, tente novamente.")
 
 ----------------------------------------------------------------------------------------------
-#4
+#5) O usuário tem 3 tentativas para acertar a senha.
+# Se errar todas, o acesso é bloqueado. Use while.
+senha = 1234
+contador = 0
+
+while contador < 3:
+    senha_correta = int(input("Insira sua senha: "))
+    if senha == senha_correta:
+        print("Welcome!")
+        break
+    else:
+        contador += 1
+        if contador == 3:
+            print("Acesso restrito")
+            break
+-----------------------------------------------------------------------------------------
+#6
