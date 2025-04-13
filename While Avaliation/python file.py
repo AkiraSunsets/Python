5b5097501604d9e61847eb090e413830022489fa
Akira Sunsets <132906123+AkiraSunsets@users.noreply.github.com>
2025-04-13 14:47:43 -0300

    Update python file.py

--- While Avaliation/python file.py
+++ While Avaliation/python file.py
@@ -105,3 +105,22 @@ for letra in frase:
         contador += 1
 
 print(f"A frase '{frase}' contém {contador} vogais")
+
+#9) Crie um programa que leia uma sequência de números e
+# determine quantos números são menores que a média.
+num = []
+size = int(input("Digite o tamanho da sequência: \n"))
+for i in range(size):
+    numero = int(input(f"Digite o {i+1}º número: "))
+    num.append(numero)
+
+menor = num[0]
+for numero in num:
+    if numero < menor:
+        menor = numero
+
+soma = sum(num)
+media = soma / len(num)
+
+print(f"O menor número é: {menor}")
+print(f"A média é {media}")
