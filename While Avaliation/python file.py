e82518a5e8a4480962a16f002cd99a3f952b8837
Akira Sunsets <132906123+AkiraSunsets@users.noreply.github.com>
2025-04-13 14:09:45 -0300

    Update python file.py

--- While Avaliation/python file.py
+++ While Avaliation/python file.py
@@ -92,3 +92,16 @@ for num in range(10):
 # Exibe as listas de números pares e ímpares
 print(f"Lista de pares: {pares}")
 print(f"Lista de ímpares: {impares}")
+
+#7) Peça uma frase e conte quantas vogais há nela.
+# Mostre o total de cada uma (a, e, i, o, u).
+
+frase = input("Digite uma frase: \n")
+vogais = "aeiouAEIOU" #o que está sendo pedido
+contador = 0 #contador de vogais
+
+for letra in frase:
+    if letra in vogais:
+        contador += 1
+
+print(f"A frase '{frase}' contém {contador} vogais")
