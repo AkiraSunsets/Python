959f0e5c7926d8511edcc0e8a980ca56b0a14192
Akira Sunsets <132906123+AkiraSunsets@users.noreply.github.com>
2025-05-14 11:20:26 -0300

    Create exercicios_while.py

---
+++ Python Luca/Aula 4 - While/exercicios_while.py
@@ -0,0 +1,22 @@
+print("Exercicios")
+
+print("Imprimir os números de 1 a 5")
+
+
+#=====================================
+print("Calcular a soma dos números de 1 a 10")
+
+
+#====================================
+print("imprimir os 5 numeros pares")
+
+
+#====================================
+print("Calcular o fatorial de um número")
+
+
+#====================================
+print("Encontrar o primeiro número da sequencia de fibonacci maior que 100")
+
+
+#====================================
