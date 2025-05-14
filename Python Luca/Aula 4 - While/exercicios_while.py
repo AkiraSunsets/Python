869f145e465df322415f5655d9bf8951967fa264
Akira Sunsets <132906123+AkiraSunsets@users.noreply.github.com>
2025-05-14 11:26:32 -0300

    Update exercicios_while.py

--- Python Luca/Aula 4 - While/exercicios_while.py
+++ Python Luca/Aula 4 - While/exercicios_while.py
@@ -1,7 +1,11 @@
 print("Exercicios")
 
 print("Imprimir os números de 1 a 5")
-
+i = 1
+while i <= 5:
+    print(i)
+    i += 1
+#(imprime lista)
 
 #=====================================
 print("Calcular a soma dos números de 1 a 10")
