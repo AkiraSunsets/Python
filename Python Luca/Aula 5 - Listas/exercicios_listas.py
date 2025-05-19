66c89bc3cbdf49ef2638b46327c61d72531042fb
Akira Sunsets <132906123+AkiraSunsets@users.noreply.github.com>
2025-05-19 11:25:02 -0300

    Create exercicios_listas.py

---
+++ Python Luca/Aula 5 - Listas/exercicios_listas.py
@@ -0,0 +1,37 @@
+print("Imprimir todos os elementos de uma lista com for Escreva um programa que receba uma lista de nomes e imprima cada nome usando um loop for.")
+nomes = ["Ana", "Bruno", "Carla", "Renata"]
+for nome in nomes:
+    print(nome)
+#=======================================================================================================================
+print("Soma dos elementos de uma lista com for Escreva um programa que receba uma lista de números e retorne a soma de todos os elementos usando um loop for.")
+lista = [1,2,3,4,5,6]
+soma = 0
+for num in lista:
+    soma += num
+print(soma)
+#=======================================================================================================================
+print("Multiplicação de todos os elementos de uma lista com for Escreva um programa que receba uma lista de números e retorne o resultado da multiplicação de todos os elementos usando um loop for.")
+lista = [1,2,3,4]
+resultado = 1
+for num in lista:
+    resultado *= num
+print(resultado)
+#=======================================================================================================================
+print("Contagem de vogais em uma lista de palavras com while Crie um programa que receba uma lista de palavras e retorne a quantidade total de vogais em todas as palavras usando um loop while.")
+palavras = ["something", "anything", "somebody", "anybody"]
+vogais = "aeiouAEIOU"
+total_vogais = 0
+index = 0
+
+while index < len(palavras):
+    palavra = palavras[index]
+    index_palavra
+
+
+#=======================================================================================================================
+print("Verificar se um elemento está presente na lista com while Crie um programa que receba uma lista de números e um número específico, e verifique se o número está presente na lista usando um loop while.")
+
+
+
+
+#=======================================================================================================================
