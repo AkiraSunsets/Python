9d315855f2cab427825b4e71f907efffabd5686d
Akira Sunsets <132906123+AkiraSunsets@users.noreply.github.com>
2025-06-02 10:01:34 -0300

    Update exemplo.py

--- Python Luca/Aula 6 - Dicionários/exemplo.py
+++ Python Luca/Aula 6 - Dicionários/exemplo.py
@@ -1,14 +1,56 @@
-print("dicionário \n")
+#Sobre as chaves
+#Devem ser unicas
+#Podem ser de qualquer tipo
 
-#É representado por chaves
-#É permitido utilizar int, float, string...
-#É utilizado para otimizar buscar
+#Sobre os dados:
+#podem ser duplicados
 
-#Ex:
 
-times_futebol = {
-    "RJ":"Flamengo", "SP":"Corinthians"
+times_futebol = {"RJ":"Flamengo","SP":"Corinthians"}
+print(times_futebol.get("RJ")) #buscar valor dentro da chave ex: flamengo
+
+
+times_futebol = {"RJ":"Flamengo","SP":"Corinthians"}
+print(times_futebol.get("BH", "chave inexistente")) #quando o
+
+times_futebol = {"RJ":"Flamengo","SP":"Corinthians"}
+print("BH" in times_futebol) #Retorna true ou false se não existir
+
+#======================================
+print("None")
+
+#Utilizado para declarar uma variavel que vc não sabe o tipo
+
+dado = None
+print(type(dado))
+dado = "s"
+print(type(dado)) #mostra o tipo do dado
+
+#=======================================================
+#exemplos dicionario
+
+sagas = {
+    (1,2):"HP",
+    (3,4):"PJ",
+    (5,6):"JV"
 }
 
-print(times_futebol)
-print(type(times_futebol))
+sagas[(7,8)] = "MR"
+sagas[(1,2)] = "Digimon"
+print(sagas) #define os dados do dicionario
+
+
+#segunda forma
+
+sagas = {
+    (1,2):"HP",
+    (3,4):"PJ",
+    (5,6):"JV"
+}
+
+dado_novo = {(7,8):"MR"}
+sagas.update(dado_novo) #atualizar dado
+sagas.update({(1,2):"Digimon"})
+print(sagas)
+
+#====================================================
