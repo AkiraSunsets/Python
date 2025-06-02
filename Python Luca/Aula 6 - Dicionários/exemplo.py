4b1c2436f0346300aa679512da31dbd2d6d5283f
Akira Sunsets <132906123+AkiraSunsets@users.noreply.github.com>
2025-06-02 09:48:11 -0300

    Create exemplo.py

---
+++ Python Luca/Aula 6 - Dicionários/exemplo.py
@@ -0,0 +1,14 @@
+print("dicionário \n")
+
+#É representado por chaves
+#É permitido utilizar int, float, string...
+#É utilizado para otimizar buscar
+
+#Ex:
+
+times_futebol = {
+    "RJ":"Flamengo", "SP":"Corinthians"
+}
+
+print(times_futebol)
+print(type(times_futebol))
